bb7abd45af8e496f3123a5c38d165908044e293d
Kishan <kishansinghthakur27@gmail.com>
2025-05-06 15:27:02 +0530

    Update resume download link in Hero component

--- components/home/hero.tsx
+++ components/home/hero.tsx
@@ -110,7 +110,7 @@ export function Hero() {
               </Button>
               <Button variant="outline" size="lg" asChild>
                 <a
-                  href="https://drive.google.com/file/d/1PbktvYeGg_1OnzT8nDH5LvS7Daxoxdw-/view?usp=drive_link"
+                  href="https://drive.google.com/file/d/122-FVkonOrCNz-eXC6v0LihZ4wONgDo8/view?usp=drive_link"
                   download="kishan_singh_thakur_Resume.pdf"
                   target="_blank"
                 >
